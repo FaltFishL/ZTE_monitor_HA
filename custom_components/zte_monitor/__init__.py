8bf4edca5ee33b4bf0c45d823468f844cdee9020
FaltFishL <FaltFishL@gmail.com>
2026-05-03 17:57:39 +0800

    Update __init__.py

--- custom_components/zte_monitor/__init__.py
+++ custom_components/zte_monitor/__init__.py
@@ -3,18 +3,13 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import (
-    DOMAIN,
-    CONF_HOST,
-    CONF_USERNAME,
-    CONF_PASSWORD,
-    CONF_REUSE_SESSION,
-    DEFAULT_REUSE_SESSION,
-    UPDATE_INTERVAL_NORMAL,
+    DOMAIN, CONF_HOST, CONF_USERNAME, CONF_PASSWORD,
+    CONF_REUSE_SESSION, DEFAULT_REUSE_SESSION, UPDATE_INTERVAL_NORMAL,
 )
 from .coordinator import ZTEMonitorCoordinator
 from .zteclient.zte_client import ZTERouterClient
 
-PLATFORMS = ["sensor", "device_tracker"]
+PLATFORMS = ["sensor", "device_tracker", "switch"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -24,16 +19,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         password=entry.data[CONF_PASSWORD],
         reuse_session=entry.options.get(CONF_REUSE_SESSION, DEFAULT_REUSE_SESSION),
     )
-
     coordinator = ZTEMonitorCoordinator(hass, client, UPDATE_INTERVAL_NORMAL)
     await coordinator.async_config_entry_first_refresh()
-
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = {
-        "client": client,
-        "coordinator": coordinator,
-    }
-
+    hass.data[DOMAIN][entry.entry_id] = {"client": client, "coordinator": coordinator}
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
@@ -41,6 +30,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
-        coordinator_data = hass.data[DOMAIN].pop(entry.entry_id)
-        await hass.async_add_executor_job(coordinator_data["client"].logout)
+        data = hass.data[DOMAIN].pop(entry.entry_id)
+        await hass.async_add_executor_job(data["client"].logout)
     return unload_ok
